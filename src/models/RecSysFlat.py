506ce5a9868558cf84e1b51bd913dd59eab160ee
Reed <aaabraha@tepper.cmu.edu>
2021-11-26 16:27:13 -0700

    IT WORKS! changed Garbage Net to emulate d2l.ai recsys net plus bias

---
+++ src/models/RecSysFlat.py
@@ -0,0 +1,38 @@
+from torch import nn
+import torch
+   
+    
+class RecSysGarbageNet(nn.Module):
+    def __init__(self, n_user, n_item):
+        super(RecSysGarbageNet, self).__init__()
+        self.alfa = nn.Parameter(torch.zeros([1,1]),requires_grad=True)
+        self.beta_u = nn.Embedding(n_user, 1)
+        self.beta_i = nn.Embedding(n_item, 1)    
+
+    def forward(self,x):
+        x = self.beta_u(x[:,0]) + self.beta_i(x[:,1])
+        out = x + self.alfa
+        return out
+    
+    
+class RecSysGarbageNetV2(nn.Module):
+    def __init__(self, n_user, n_item, n_factor):
+        super(RecSysGarbageNetV2, self).__init__()
+        
+        # rec matrices
+        self.P = nn.Embedding(n_user, n_factor)
+        self.Q = nn.Embedding(n_item, n_factor)
+        
+        # biases
+        self.beta_u = nn.Embedding(n_user, 1)
+        self.beta_i = nn.Embedding(n_item, 1)            
+        self.alfa = nn.Parameter(torch.zeros([1,1]),requires_grad=True)
+
+    def forward(self,x):        
+        P_u = self.P(x[:,0]).squeeze()
+        Q_i = self.Q(x[:,1]).squeeze()
+        b_u = self.beta_u(x[:,0]).squeeze()
+        b_i = self.beta_i(x[:,1]).squeeze()
+        alfa = self.alfa
+        outputs = (P_u * Q_i).sum(axis=1) + b_u + b_i + alfa
+        return outputs.flatten()
--- src/rec_sys_garbagio_driver_2.py
+++ src/rec_sys_garbagio_driver_2.py
@@ -1,4 +1,5 @@
 import data_mgmt.RecSysData as rsd
+from models.RecSysFlat import RecSysGarbageNetV2
 from torch.utils.data import DataLoader
 import torch
 from torch import nn
@@ -36,19 +37,6 @@ if we set this higher we may get better results.
 Little too tired to think that through right now.
 
 """
-
-
-class RecSysGarbageNet(nn.Module):
-    def __init__(self, n_user, n_item):
-        super(RecSysGarbageNet, self).__init__()
-        self.alfa = nn.Parameter(torch.zeros([1,1]),requires_grad=True)
-        self.beta_u = nn.Embedding(n_user, 1)
-        self.beta_i = nn.Embedding(n_item, 1)    
-
-    def forward(self,x):
-        x = self.beta_u(x[:,0]) + self.beta_i(x[:,1])
-        out = x + self.alfa
-        return out
     
 def train_loop(dataloader, model, loss_fn, optimizer):
     size = len(dataloader.dataset)
@@ -76,21 +64,21 @@ def train_loop(dataloader, model, loss_fn, optimizer):
             t.set_postfix(current_loss=loss.item(), refresh=False)
             t.update()
         
-        print(f"Epoch Done, max loss:{max_loss}, min loss: {min_loss}, final loss: {loss.item()}")
+        print(f"\nEpoch Done, max loss:{max_loss:.3f}, min loss: {min_loss:.3f}, final loss: {loss.item():.3f}")
     
 def main():
     ppath=r"C:\git\cmu_msba_2022_ml_applications_2\data"+"\\"
     omfg = rsd.RecSysData(ppath)
-    tdl = DataLoader(omfg, batch_size=4000, shuffle=True)
+    tdl = DataLoader(omfg, batch_size=10000, shuffle=True)
 
     n_user = omfg.df_data.uid.unique().shape[0]
     n_item = omfg.df_data.pid.unique().shape[0]
 
-    learning_rate = 1e-1
-    model = RecSysGarbageNet(n_user,n_item)
+    learning_rate = 1.0
+    model = RecSysGarbageNetV2(n_user,n_item,300)
     loss = torch.nn.MSELoss()
-    optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
-    #optimizer = torch.optim.Adadelta(model.parameters(), lr=10.0, rho=0.9, eps=1e-06, weight_decay=0)
+    #optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
+    optimizer = torch.optim.Adadelta(model.parameters(), lr=10.0, rho=0.9, eps=1e-06, weight_decay=0)
     scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.92, verbose=True)
     
     epochs = 100
