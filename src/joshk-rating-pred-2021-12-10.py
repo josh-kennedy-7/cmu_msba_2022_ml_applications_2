c18f1784ee58c1714d4658ecfba83169d7ea31d1
Josh Kennedy <jtkennedy7@gmail.com>
2021-12-19 14:41:26 -0800

    Joshk files

--- src/joshk-rating-pred-2021-12-10.py
+++ src/joshk-rating-pred-2021-12-10.py
@@ -11,6 +11,8 @@ import time
 from copy import deepcopy
 from data_mgmt.BaseDataClass import BaseDataClass
 
+# try this: SVDpp
+
 from surprise import SVD
 from surprise import SVDpp
 from surprise import accuracy
@@ -21,7 +23,7 @@ from surprise import BaselineOnly
 rawdf = BaseDataClass._loadUpDf('/Users/joshkennedy/Documents/CMU/ML for Business Applications 2/','train','.json')
 rawdf['date'] = rawdf['reviewTime']
 df = rawdf.copy()
-
+df.to_csv('clean_df.csv')
 
 SAMPLING_RATE = 5/5
 user_id_unique = df['reviewerID'].unique()
@@ -30,11 +32,18 @@ user_id_sample = pd.DataFrame(user_id_unique, columns=['unique_user_id']) \
 ratings_sample = df.merge(user_id_sample, left_on='reviewerID', right_on='unique_user_id') \
                     .drop(['unique_user_id'], axis=1)
 
+all_users = pd.DataFrame(df.groupby('reviewerID').count()['rating']).reset_index()
+all_users.columns = ['reviewerID','num_ratings']
+users_more_than_2_purchases = all_users[all_users['num_ratings'] > 2].copy()
+users_more_than_5_purchases = all_users[all_users['num_ratings'] > 5].copy()
+avg_item_ratings_dict = dict(df.groupby('itemID').mean()['rating'])
+num_item_ratings_dict = dict(df.groupby('itemID').count()['rating'])
+num_user_ratings_dict = dict(df.groupby('reviewerID').count()['rating'])
 
 # most recent review is testset, second most recent is val, rest are train
 ratings_sample['review_date_rank'] = ratings_sample.groupby('reviewerID')['date'].rank(ascending=False)
-testset = ratings_sample[ratings_sample['review_date_rank'] == 1].copy()
-valset = ratings_sample[ratings_sample['review_date_rank'] == 2].copy()
+testset = ratings_sample[ratings_sample['review_date_rank'] == 2].copy()
+valset = ratings_sample[ratings_sample['review_date_rank'] == 1].copy()
 trainset = ratings_sample[ratings_sample['review_date_rank'] > 2].copy()
 
 reader = Reader(rating_scale = (1.0, 5.0))
@@ -51,9 +60,9 @@ bias_baseline = BaselineOnly(bsl_options)
 bias_baseline.fit(train_sr)
 predictions = bias_baseline.test(val_sr)
 
-min(list(RMSE_tune.values()))
+
 RMSE_tune = {}
-n_epochs = [10, 15, 25, 50]  # the number of iteration of the SGD procedure
+n_epochs = [10, 15, 25]  # the number of iteration of the SGD procedure
 lr_all = [0.002, 0.003, 0.005, 0.008] # the learning rate for all parameters
 reg_all =  [0.4, 0.5, 0.6, 0.7, 0.8] # the regularization term for all parameters
 # n_epochs = [25]  # the number of iteration of the SGD procedure
@@ -63,11 +72,16 @@ for n in n_epochs:
     for l in lr_all:
         for r in reg_all:
             print(f'Starting n={n}, l={l}, r={r}')
-            algo = SVD(n_epochs = n, lr_all = l, reg_all = r)
+            algo = SVDpp(n_epochs = n,  &mowitlr_all = l, reg_all = r)
             algo.fit(train_sr)
             predictions = algo.test(val_sr)
             RMSE_tune[n,l,r] = accuracy.rmse(predictions)
-algo_real = SVD(n_epochs = 25, lr_all = 0.008, reg_all = 0.8)
+
+min(RMSE_tune.values())
+
+algo_real = SVD(n_epochs = 25, lr_all = 0.008, reg_all = 0.8) # SVD best
+# algo_real = SVDpp(n_epochs = 50, lr_all = 0.003, reg_all = 0.8) # SVDpp best
+# algo_real = SVDpp(n_epochs = 25, lr_all = 0.005, reg_all = 0.5) # SVDpp best after switching train/val set
 algo_real.fit(train_sr)
 predictions = algo_real.test(test_sr)
 accuracy.rmse(predictions)
@@ -85,11 +99,18 @@ actual_pred_dict = {}
 for i in actual_predictions:
     actual_pred_dict[i[0]] = i[3]
 
+def user_pred_function(row):
+    user = row['reviewerID']
+    item = row['itemID']
+    try:
+        num_user_ratings = num_user_ratings_dict[user]
+    except:
+        num_user_ratings = 0
+    if (num_user_ratings <= 2) and (item in list(df['itemID'].unique())) and (num_item_ratings_dict[item] > 5):
+        return avg_item_ratings_dict[item]
+    else:
+        return actual_pred_dict[user]
+
 actual_test_output = actualtestset.copy()
-actual_test_output['prediction'] = actual_test_output['reviewerID'].map(actual_pred_dict)
-actual_test_output[['reviewerID-itemID','prediction']].to_csv('jk-rating-output-2021-12-10-v4.csv')
-
-
-actual_test_output[~actual_test_output['reviewerID'].isin(ratings_sample['reviewerID'])]
-
-actual_test_output[~actual_test_output['itemID'].isin(ratings_sample['itemID'])]
+actual_test_output['prediction'] = actual_test_output.apply(user_pred_function, axis=1)
+actual_test_output[['reviewerID-itemID','prediction']].to_csv('jk-rating-output-2021-12-12-v12.csv')
